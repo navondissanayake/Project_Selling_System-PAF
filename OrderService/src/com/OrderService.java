13e7f81aed31d59628c8c2456d7985b88c51351c
Piumi <aluviharepiumee@gmail.com>
2021-04-19 14:50:19 +0530

    initial commit

---
+++ OrderService/src/com/OrderService.java
@@ -0,0 +1,128 @@
+package com;
+
+import javax.ws.rs.Consumes;
+
+
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*;
+
+//For XML
+	import org.jsoup.*;
+	import org.jsoup.parser.*;
+	import org.jsoup.nodes.Document;
+
+
+import model.Order;
+
+@Path("/Order")
+public class OrderService {
+	
+	
+	Order ordObj = new Order();
+
+	
+	
+	//view order details
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readOrders()
+	 {
+		return ordObj.readOrders(); 
+	 } 
+	
+	
+	
+	
+	
+	
+	//view each buyer order details
+	@GET
+	@Path("/{BuyerID}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String readOrdersbuyer(@PathParam("BuyerID") String BuyerID)
+	 {
+		return ordObj.readOrdersbuyer(BuyerID); 
+	 }
+	
+	
+
+	
+	
+	
+	
+	
+	//insert orders
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertOrder(
+	 @FormParam("Name") String name,
+	 @FormParam("BuyerID") String buyerID,
+	 @FormParam("productID") String productID,
+	 @FormParam("Price") String price,
+	 @FormParam("qty") int qty)
+	
+	{
+	 String output = ordObj.insertOrder(name,buyerID, productID, price,qty);
+	return output;
+	}
+	
+	
+	
+	
+	
+	//update order details
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateItem(String itemData)
+	{
+	//Convert the input string to a JSON object
+	 JsonObject itemObject = new JsonParser().parse(itemData).getAsJsonObject();
+	//Read the values from the JSON object
+	 String orderID = itemObject.get("orderID").getAsString();
+	 int qty = Integer.parseInt(itemObject.get("qty").getAsString());
+
+	 String output = ordObj.updateOrderQuantity(orderID, qty);
+	 
+	 
+	 return output;
+	 
+	}
+	
+	
+	
+	
+	
+	
+	//delete order details
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteItem(String itemData)
+	{
+	//Convert the input string to an XML document
+	 Document doc = Jsoup.parse(itemData, "", Parser.xmlParser());
+
+	//Read the value from the element <itemID>
+	 String orderID = doc.select("orderID").text();
+	 String output = ordObj.deleteOrders(orderID);
+	return output;
+	}
+
+}
---
+++ OrderService/src/model/Order.java
@@ -0,0 +1,405 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+
+
+public class Order {
+	
+	
+	    //A common method to connect to the DB
+		private Connection connect(){
+			
+			Connection con = null;
+			
+			try
+			{
+				
+				Class.forName("com.mysql.jdbc.Driver");
+				//Provide the correct details: DBServer/DBName, username, password
+				con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gadgetbadget","root", "1999ppdd");
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				
+			
+			}
+			
+		    return con;
+		}
+	
+		  
+	  
+		
+		
+		
+		 public String insertOrder(String name ,String buyerID, String productID, String price , int qty  ) {
+		{
+		
+			String output = "";
+	
+			int items_qty=0;
+			boolean validate = false;
+			java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());
+			
+		
+			try
+			{
+				
+				Connection con = connect();
+				
+				if (con == null)
+				{
+					return "Error while connecting to the database for inserting."; 
+			
+			
+				}
+		
+				String query2 = " select Stock_qty from items where Item_id = ?";
+				PreparedStatement preparedStmt2 = con.prepareStatement(query2);
+				preparedStmt2.setString(1, productID);
+				ResultSet rs = preparedStmt2.executeQuery();
+				
+				
+	
+				while(rs.next()) {
+					items_qty= rs.getInt("Stock_qty");
+		
+				}
+	
+	
+				if(qty > items_qty) {
+					
+					validate = false;
+					output = " only " + items_qty + " quantity available!" ;
+				}
+				
+				
+				else {
+					validate = true;
+				}
+				
+				
+				if(validate) {
+				
+		
+	
+	        
+				// create a prepared statement
+				String query = " insert into orders(`orderID`,`Name`,`BuyerID`,`productID`,`Price`,`qty`,`Date`)"+ "values(?,?,?,?,?,?,?)";
+				
+				PreparedStatement preparedStmt = con.prepareStatement(query);
+				
+		
+				
+				// binding values
+				preparedStmt.setInt(1, 0);
+				preparedStmt.setString(2, name);
+				preparedStmt.setString(3, buyerID);
+				preparedStmt.setString(4, productID);
+				preparedStmt.setDouble(5, Double.parseDouble(price));
+				preparedStmt.setInt(6, qty);
+			    preparedStmt.setDate(7, sqlDate);
+				
+				
+				// execute the statement
+		
+				preparedStmt.execute();
+				con.close();
+				output = "Inserted orders successfully";
+				
+					}
+				}
+			
+				catch (Exception e)
+				{
+				output = "Error while inserting the orders.";
+				System.err.println(e.getMessage());
+				}
+			
+				return output;
+				}
+			
+			}
+	  
+	  
+	    
+	
+	//read orders
+	public String readOrders() {
+		
+		
+		String output = "";
+		
+		
+		
+		try {
+			
+			
+			
+			Connection con = connect();
+			
+			
+			if (con == null) {
+				
+				return "Error while connecting to the database for reading.";
+			}
+			
+			
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr><th>Order ID</th><th>Name</th><th>Buyer ID</th>" + "<th>Product ID</th>"
+					+ "<th>Price</th>"+ "<th>Quantity</th>" + "<th>Order Date</th>" + "<th>Update</th><th>Remove</th></tr>";
+
+			
+			String query = "select * from orders";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+
+			
+			
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				String orderID = Integer.toString(rs.getInt("orderID"));
+				String name = rs.getString("Name");
+				String buyerID = rs.getString("BuyerID");
+				String productID = rs.getString("productID");
+				String price = Double.toString(rs.getDouble("Price")); 
+				String qty = Integer.toString(rs.getInt("qty"));
+				String orderDate = rs.getString("Date");
+				
+
+				
+				// Add into the html table
+				output += "<tr><td>" + orderID + "</td>";
+				output += "<td>" + name + "</td>";
+				output += "<td>" + buyerID + "</td>";
+				output += "<td>" + productID + "</td>";
+				output += "<td>" + price + "</td>";
+				output += "<td>" + qty + "</td>";
+				output += "<td>" + orderDate + "</td>";
+
+				// buttons
+				output += "<td><input name='btnUpdate' type='button' value='Update'class='btn btn-secondary'></td>"+ "<td><form method='post' action='items.jsp'>"+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
+						+ "<input name='orderID' type='hidden' value='" + orderID + "'>" + "</form></td></tr>";
+			}
+			con.close();
+			
+
+			// Complete the html table
+			output += "</table>";
+			
+			
+		} catch (Exception e) {
+			
+			output = "Error while reading the orders.";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+	
+	//view each  buyer order details
+	public String readOrdersbuyer(String BuyerID){
+		
+		
+		String output = "";
+		
+		
+		try {
+			
+			
+			Connection con = connect();
+			
+			
+			if (con == null) {
+				
+				
+				return "Error while connecting to the database for reading.";
+			}
+			
+			
+			
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr><th>Order ID</th><th>Name</th><th>Buyer ID</th><th>Product ID</th>"+ "<th>Price</th>" + "<th>Quantity</th>" + "<th>Order Date</th>" +"<th>Update</th><th>Remove</th></tr>";
+
+			
+			//String query = "select * from orders where BuyerID =?";
+			
+			//String query = "select * from orders where BuyerID=' " + BuyerID + "'" ;
+			
+			//Statement stmt = con.createStatement();
+		   // ResultSet rs = stmt.executeQuery(query);
+
+			String query = "select * from orders where BuyerID =?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			preparedStmt.setString(1, BuyerID);
+			ResultSet rs = preparedStmt.executeQuery();
+
+		    
+		  
+			
+			// iterate through the rows in the result set
+			
+			
+			while (rs.next()) {
+				
+				
+				String orderID = Integer.toString(rs.getInt("orderID"));
+				String name = rs.getString("Name");
+				String buyerID = rs.getString("BuyerID");
+				String productID = rs.getString("productID");
+				String price = Double.toString(rs.getDouble("Price")); 
+				String qty = Integer.toString(rs.getInt("qty"));
+				String orderDate = rs.getString("Date");
+				
+				
+				
+
+				// Add into the html table
+				output += "<tr><td>" + orderID + "</td>";
+				output += "<td>" + name + "</td>";
+				output += "<td>" + buyerID + "</td>";
+				output += "<td>" + productID + "</td>";
+				output += "<td>" + price + "</td>";
+				output += "<td>" + qty + "</td>";
+				output += "<td>" + orderDate + "</td>";
+
+				
+				
+				
+				// buttons
+				output += "<td><input name='btnUpdate' type='button' value='Update'class='btn btn-secondary'></td>"
+						+ "<td><form method='post' action='items.jsp'>"
+						+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
+						+ "<input name='orderID' type='hidden' value='" + orderID + "'>" + "</form></td></tr>";
+			}
+			
+			con.close();
+
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the orders.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+	
+
+	
+
+		
+	
+	
+	
+	//update order
+	public String updateOrderQuantity(String ID, int qty) {
+		
+		
+		String output = "";
+		try {
+			
+			
+			Connection con = connect();
+			
+			
+			if (con == null) {
+				return "Error while connecting to the database for updating.";
+			}
+			
+			
+			// create a prepared statement
+			String query = "UPDATE orders SET qty=? WHERE orderID=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			
+			// binding values
+			preparedStmt.setInt(1, qty);
+			preparedStmt.setString(2, ID);
+
+			
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			
+			output = "Updated successfully";
+			
+		} catch (Exception e) {
+			output = "Error while updating the order.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//delete order
+	public String deleteOrders(String ID) {
+		
+		String output = "";
+		
+		try {
+			
+			Connection con = connect();
+			
+			if (con == null) {
+				return "Error while connecting to the database for deleting.";
+			}
+			
+			// create a prepared statement
+			String query = "delete from orders where orderID=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(ID));
+			
+			// execute the statement
+			preparedStmt.execute();
+			
+			con.close();
+			
+			output = "Deleted successfully";
+			
+			
+		} catch (Exception e) {
+			
+			output = "Error while deleting the order.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+	
+	
+
+
+
+}
